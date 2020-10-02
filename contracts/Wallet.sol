bce97e84d49d416755ab802dc9cf59124b0db81c
zaryab2000 <zaryabafser2000@gmail.com>
2020-10-02 10:16:41 +0530

    initial commit

--- contracts/Wallet.sol
+++ contracts/Wallet.sol
@@ -1,24 +1,6 @@
-// pragma solidity >= 0.5.0 < 0.7.0;
-pragma solidity ^0.5.0;
+pragma solidity >= 0.5.0 < 0.7.0;
 
 
 contract Wallet{
-  uint storedData;
-  uint _myVal;
-
-  constructor(){
-  	_myVal =100;
-  }
-
-  function set(uint x) public {
-    storedData = x;
-  }
-
-  function  getMyVal() public view returns(uint){
-  	return _myVal;
-  }
   
-  function get() public view returns (uint) {
-    return storedData;
-  }
 }
